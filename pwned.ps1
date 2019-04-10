59b5722ea98e3b3022226dcb3ff68e88e1321ba4
R3ality <idle.relay@gmail.com>
2019-04-10 20:20:16 +0300

    Wrap in while loop to allow checking multiple passwords

--- pwned.ps1
+++ pwned.ps1
@@ -1,24 +1,34 @@
-$string = Read-Host -Prompt 'Password to check'
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($string)
-$sha1 = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider
-$data = $sha1.ComputeHash($bytes)
-$result = ($data | ForEach-Object ToString X2) -join ''
-$head = $result.Substring(0,5)
-$tail = $result.Substring(5)
+Write-Host 'Checks first 5 characters of the SHA1 hash against haveibeenpwned.com API.'
+Write-Host 'Enter an empty password or close the window to exit.'
 
-[Net.ServicePointManager]::SecurityProtocol = "TLS12, TLS11, TLS, SSL3"
-$request = [System.Net.WebRequest]::Create("https://api.pwnedpasswords.com/range/" + $head)
-$reader = New-Object System.IO.StreamReader(($request.GetResponse()).GetResponseStream())
-
-$found = 0
-while ($null -ne ($line = $reader.ReadLine())) {
-    if (($split = $line.Split(':'))[0] -eq $tail) {
-        Write-Host "That password has been compromised. Occurrences:" $split[1]
-        $found = 1
-        break
+while ($true) {
+    $string = Read-Host -Prompt "`nPassword to check"
+    
+    if (-not $string) {
+        Write-Host 'No password entered. Exiting..'
+        exit
     }
-}
-if ($found -eq 0) { Write-Host "That password was not found." }
 
-$reader.Close()
-Read-Host -Prompt "Press Enter to exit"
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($string)
+    $sha1 = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider
+    $data = $sha1.ComputeHash($bytes)
+    $result = ($data | ForEach-Object ToString X2) -join ''
+    $head = $result.Substring(0,5)
+    $tail = $result.Substring(5)
+
+    [Net.ServicePointManager]::SecurityProtocol = "TLS12, TLS11, TLS, SSL3"
+    $request = [System.Net.WebRequest]::Create("https://api.pwnedpasswords.com/range/" + $head)
+    $reader = New-Object System.IO.StreamReader(($request.GetResponse()).GetResponseStream())
+
+    $found = 0
+    while ($null -ne ($line = $reader.ReadLine())) {
+        if (($split = $line.Split(':'))[0] -eq $tail) {
+            Write-Host "That password has been compromised. Occurrences:" $split[1]
+            $found = 1
+            break
+        }
+    }
+    if ($found -eq 0) { Write-Host "That password was not found." }
+
+    $reader.Close()
+}
