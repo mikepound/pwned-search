08ae4caf2ff0cf5942c6e86daf8a71c53bc2259c
Mo’ops <Ethical.Hacker720@gmail.com>
2019-05-09 02:51:06 -0400

    Make it more readable

--- pwned.py
+++ pwned.py
@@ -1,62 +1,68 @@
 #!/usr/bin/env python
-import hashlib
-import sys
+
+from hashlib import sha1
+from getpass import getpass
 
 try:
-    import requests
+    from requests import get
 except ModuleNotFoundError:
     print("###  pip install requests  ###")
     raise
 
 
-def lookup_pwned_api(pwd):
-    """Returns hash and number of times password was seen in pwned database.
+class Pwned:
 
-    Args:
-        pwd: password to check
+    def __init__(self):
+        self.api = 'https://api.pwnedpasswords.com/range/'
 
-    Returns:
-        A (sha1, count) tuple where sha1 is SHA-1 hash of pwd and count is number
-        of times the password was seen in the pwned database.  count equal zero
-        indicates that password has not been found.
+    def hash_pwd(self, pwd):
+        hash = sha1(pwd.encode()).hexdigest().upper()
 
-    Raises:
-        RuntimeError: if there was an error trying to fetch data from pwned
-            database.
-        UnicodeError: if there was an error UTF_encoding the password.
-    """
-    sha1pwd = hashlib.sha1(pwd.encode('utf-8')).hexdigest().upper()
-    head, tail = sha1pwd[:5], sha1pwd[5:]
-    url = 'https://api.pwnedpasswords.com/range/' + head
-    res = requests.get(url)
-    if res.status_code != 200:
-        raise RuntimeError('Error fetching "{}": {}'.format(
-            url, res.status_code))
-    hashes = (line.split(':') for line in res.text.splitlines())
-    count = next((int(count) for t, count in hashes if t == tail), 0)
-    return sha1pwd, count
+        return {
+            'hash_full': hash,
+            'hash_head': hash[:5],
+            'hash_tail': hash[5:]
+        }
 
+    def request_api(self, hash_head):
+        failed = True
 
-def main(args):
-    ec = 0
-    for pwd in args or sys.stdin:
-        pwd = pwd.strip()
         try:
-            sha1pwd, count = lookup_pwned_api(pwd)
+            resp = get(self.api + hash_head)
+            if resp.status_code == 200:
+                failed = False
+        except:
+            pass
+        finally:
+            if failed:
+                raise RuntimeError('Failed to fetch data')
+            return resp.text
 
-            if count:
-                foundmsg = "{0} was found with {1} occurrences (hash: {2})"
-                print(foundmsg.format(pwd, count, sha1pwd))
-                ec = 1
-            else:
-                print("{} was not found".format(pwd))
-        except UnicodeError:
-            errormsg = sys.exc_info()[1]
-            print("{0} could not be checked: {1}".format(pwd, errormsg))
-            ec = 1
-            continue
-    return ec
+    def find_match(self, resp, hash_tail):
+        data = [int(item.split(':')[-1])
+                for item in resp.splitlines() if item.split(':')[0] == hash_tail]
+
+        if data:
+            return data[0]
+
+    def lookup_pwned_api(self, pwd):
+
+        hash_info = self.hash_pwd(pwd)
+        resp = self.request_api(hash_info['hash_head'])
+        amount = self.find_match(resp, hash_info['hash_tail'])
+
+        return hash_info['hash_full'].lower(), amount
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    pwd = getpass('Enter password: ')
+
+    pwned = Pwned()
+    hash, amount = pwned.lookup_pwned_api(pwd)
+
+    if amount:
+        print('Password was found with {} occurrences (hash: {})'.format(
+            amount, hash
+        ))
+    else:
+        print('Password was not found')
