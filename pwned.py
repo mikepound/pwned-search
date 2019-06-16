cb4fd9cd833547990f67467f6e21b1e44edd1857
Alex Povel <mail@alexpovel.de>
2019-06-16 11:50:05 +0200

    Tighter try/except handling\nPutting less code into the try block

--- pwned.py
+++ pwned.py
@@ -43,18 +43,18 @@ def main(args):
         pwd = pwd.strip()
         try:
             sha1pwd, count = lookup_pwned_api(pwd)
-
-            if count:
-                foundmsg = "{0} was found with {1} occurrences (hash: {2})"
-                print(foundmsg.format(pwd, count, sha1pwd))
-                ec = 1
-            else:
-                print("{} was not found".format(pwd))
         except UnicodeError:
             errormsg = sys.exc_info()[1]
             print("{0} could not be checked: {1}".format(pwd, errormsg))
             ec = 1
             continue
+
+        if count:
+            foundmsg = "{0} was found with {1} occurrences (hash: {2})"
+            print(foundmsg.format(pwd, count, sha1pwd))
+            ec = 1
+        else:
+            print("{} was not found".format(pwd))
     return ec
 
 
