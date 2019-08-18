71c941f1e1f9275a5ea023a1bcda6d3d5b406c0a
David Bors <daviddvd267@gmail.com>
2019-08-18 14:51:10 +0300

    Added Haskell version.

--- pwned.hs
+++ pwned.hs
@@ -2,6 +2,7 @@
 -- Usage of the works is permitted provided that this instrument is retained
 -- with the works, so that any entity that uses the works is notified of this instrument
 -- DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
+-- Usage: runghc pwned.hs <password that you want to try>
 
 {-# LANGUAGE OverloadedStrings #-}
 
